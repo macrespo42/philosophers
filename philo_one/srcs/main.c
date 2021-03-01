42418249bed818ef5aaa67128f7f4e3cd6ae9663
macrespo42 <maxime.crespopro@gmail.com>
2021-03-01 10:41:07 +0100

    p1 init threads

--- philo_one/srcs/main.c
+++ philo_one/srcs/main.c
@@ -6,17 +6,30 @@
 /*   By: macrespo <macrespo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/20 11:15:12 by macrespo          #+#    #+#             */
-/*   Updated: 2021/02/26 15:17:34 by macrespo         ###   ########.fr       */
+/*   Updated: 2021/03/01 10:40:50 by macrespo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo_one.h"
+
+void	*live(void *p_data)
+{
+	t_philo *philo;
+
+	philo = (t_philo*)p_data;
+	printf("I'm the thread nb : %d\n", philo->id);
+	pthread_mutex_lock(&philo->fork);
+	//feed the philo
+	pthread_mutex_unlock(&philo->fork);
+	return (philo);
+}
 
 int		main(int ac, char **av)
 {
 	t_args		args;
 	t_timeval	current_time;
 	t_philo		*head;
+	t_philo *philos;
 
 	if (ac >= 5 && ac < 7)
 	{
@@ -24,9 +37,22 @@ int		main(int ac, char **av)
 			return (print_error("Error: bad arguments"));
 		gettimeofday(&current_time, NULL);
 		head = init_philos(args);
+		int i;
+		i = 0;
+		philos = head;
+		while (i < args.philos_nb)
+		{
+			if (i % 0 != 0)
+				usleep(50);
+			pthread_mutex_init(&philos->fork, NULL);
+			pthread_create(&philos->philo_pid, NULL, live, philos);
+			philos = philos->next;
+			i++;
+		}
 		free_philos(head, args);
 	}
 	else
 		return (print_error("Error: bad numbers of arguments"));
+	usleep(5000);
 	return (EXIT_SUCCESS);
 }
