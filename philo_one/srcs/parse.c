ccd037d5cdfc840b45a93879602845470a672738
macrespo42 <maxime.crespopro@gmail.com>
2021-03-08 15:19:13 +0100

    if time_must_eat not define set default to -1

--- philo_one/srcs/parse.c
+++ philo_one/srcs/parse.c
@@ -6,7 +6,7 @@
 /*   By: macrespo <macrespo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/27 14:47:52 by macrespo          #+#    #+#             */
-/*   Updated: 2021/03/03 13:20:33 by macrespo         ###   ########.fr       */
+/*   Updated: 2021/03/08 15:17:51 by macrespo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,13 @@ int	get_philo_infos(int ac, char **av, t_args *args)
 	args->time_to_sleep = ft_atoi(av[4]);
 	args->all_alive = 1;
 	if (ac == 6)
-	{
-		args->must_eat_defined = 1;
 		args->time_must_eat = ft_atoi(av[5]);
-	}
-	else
-		args->must_eat_defined = 0;
 	if (args->philos_nb <= 0 || args->time_to_die < 0 ||
 		args->time_to_eat < 0 || args->time_to_sleep < 0)
 		return (EXIT_FAILURE);
-	if (args->must_eat_defined == 1 && args->time_must_eat <= 0)
+	if (args->time_must_eat <= 0)
 		return (EXIT_FAILURE);
+	if (ac < 6)
+		args->time_must_eat = -1;
 	return (EXIT_SUCCESS);
 }
