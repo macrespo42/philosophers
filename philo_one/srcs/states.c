b356d0b6e1c7d53066be0d20548e4a5165a52554
macrespo42 <maxime.crespopro@gmail.com>
2021-03-16 14:09:22 +0100

    fix last meal init timing

--- philo_one/srcs/states.c
+++ philo_one/srcs/states.c
@@ -6,7 +6,7 @@
 /*   By: macrespo <macrespo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/03 15:33:23 by macrespo          #+#    #+#             */
-/*   Updated: 2021/03/15 09:35:38 by macrespo         ###   ########.fr       */
+/*   Updated: 2021/03/16 13:53:27 by macrespo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,13 @@ void		print_state(char *action, useconds_t delay, t_philo *philo)
 {
 	long	timestamp;
 
-	pthread_mutex_lock(philo->args->printer);
 	timestamp = get_tv_msec() - philo->args->initial_time;
+	pthread_mutex_lock(philo->args->printer);
 	if (!philo->args->death_flag)
 		printf("[%ld] %d %s\n", timestamp, philo->id, action);
+	pthread_mutex_unlock(philo->args->printer);
 	if (philo->state == DEAD)
 		philo->args->death_flag = 1;
-	pthread_mutex_unlock(philo->args->printer);
 	if (delay > 0)
 		ft_msleep(delay);
 }
@@ -35,10 +35,10 @@ void		eat(t_philo *philo)
 	pthread_mutex_lock(&philo->next->fork);
 	print_state("has taken a fork", 0, philo);
 	philo->state = EATING;
+	philo->last_meal = get_tv_msec();
 	print_state("is eating", philo->args->time_to_eat, philo);
 	pthread_mutex_unlock(&philo->next->fork);
 	pthread_mutex_unlock(&philo->fork);
-	philo->last_meal = get_tv_msec();
 	philo->state = SLEEPING;
 }
 
@@ -49,7 +49,7 @@ void		*routine(void *p_data)
 	philo = (t_philo*)p_data;
 	if (philo->id % 2 == 0)
 		ft_msleep(philo->args->time_to_eat);
-	philo->last_meal = philo->args->initial_time;
+	philo->last_meal = get_tv_msec();
 	while (philo->alive && (philo->args->time_must_eat == -1
 	|| (philo->args->time_must_eat != -1
 	&& philo->eat_times < philo->args->time_must_eat)))
