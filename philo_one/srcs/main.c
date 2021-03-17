5704cce8c1b131822403ab913901d54772a119d3
macrespo42 <maxime.crespopro@gmail.com>
2021-03-17 14:36:39 +0100

    strange ts seems to be gone

--- philo_one/srcs/main.c
+++ philo_one/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: macrespo <macrespo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/20 11:15:12 by macrespo          #+#    #+#             */
-/*   Updated: 2021/03/17 10:50:21 by macrespo         ###   ########.fr       */
+/*   Updated: 2021/03/17 14:32:37 by macrespo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,6 @@ void			*supervisor(void *p_data)
 	tmp = (t_philo*)p_data;
 	while (tmp->next != NULL && tmp->alive)
 	{
-		// printf("Totals meal : %d\n", tmp->args->total_meal);
 		now = get_tv_msec();
 		if (tmp->first_meal != 0 &&
 		now - tmp->last_meal > tmp->args->time_to_die)
@@ -47,7 +46,10 @@ void			*supervisor(void *p_data)
 			print_state("is dead", 0, tmp);
 		}
 		if (tmp->args->total_meal == tmp->args->philos_nb)
-			return (tmp);
+		{
+			tmp->alive = 0;
+			tmp->state = DEAD;
+		}
 		tmp = tmp->next;
 	}
 	return (tmp);
