16a22cbc76ec9d3868d67b948d03320362b7525d
macrespo42 <maxime.crespopro@gmail.com>
2021-03-22 14:43:38 +0100

    nb of eat reduce meal

--- philo_two/srcs/states.c
+++ philo_two/srcs/states.c
@@ -6,7 +6,7 @@
 /*   By: macrespo <macrespo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/03 15:33:23 by macrespo          #+#    #+#             */
-/*   Updated: 2021/03/18 14:28:00 by macrespo         ###   ########.fr       */
+/*   Updated: 2021/03/22 14:17:06 by macrespo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,7 @@ void		eat(t_philo *philo)
 	sem_post(philo->args->forks);
 	sem_post(philo->args->forks);
 	philo->state = SLEEPING;
-	if (philo->eat_times == philo->args->time_must_eat)
+	if (philo->eat_times == philo->args->time_must_eat - 1)
 		philo->args->total_meal += 1;
 	philo->eat_times += 1;
 }
@@ -51,7 +51,7 @@ void		*routine(void *p_data)
 
 	philo = (t_philo*)p_data;
 	if (philo->id % 2 == 0)
-		ft_msleep(philo->args->time_to_eat);
+		ft_msleep(0.9 * (float)philo->args->time_to_eat);
 	philo->last_meal = get_tv_msec();
 	while (philo->alive)
 	{
